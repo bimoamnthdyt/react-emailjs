b025bcc1f4035b82c46c39efacdff52391cdc7a2
bimoamnthdyt <bimoamantahidayat01@gmail.com>
2023-03-14 10:29:50 +0700

    finishing

---
+++ src/components/Contact.jsx
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contactus = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("YOUR_SERVICE_id", "YOUR_TEMPLATE_id", form.current, "YOUR_PUBLIC_key").then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+
+  return (
+    <div className="w-full h-screen text-center flex flex-col justify-center">
+      <form className="max-w-sm mx-auto" ref={form} onSubmit={sendEmail}>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Full Name
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="user_name" type="text" />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Email
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="user_email" type="text" />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Message
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <textarea
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              name="message"
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+        </div>
+
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div>
+            <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+              Send
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Contactus;
